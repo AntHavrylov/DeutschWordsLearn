960fe6b93a95167fdcccfb14dc17fda210b0ed3b
Your Name <ant.havrylov@gmail.com>
2025-08-24 01:03:37 +0200

    all colours setting extracted to styles.css file for the following changes

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit(): void {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      this.document.body.classList.add(savedTheme);
+    }
+  }
+
+  toggleTheme(): void {
+    if (this.document.body.classList.contains('high-contrast')) {
+      this.document.body.classList.remove('high-contrast');
+      localStorage.removeItem('theme');
+    } else {
+      this.document.body.classList.add('high-contrast');
+      localStorage.setItem('theme', 'high-contrast');
+    }
+  }
 }
