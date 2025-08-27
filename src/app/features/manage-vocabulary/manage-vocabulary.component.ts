1b8ff7dcda7d83851eeed7acd052aa1be628ecdf
Anton Havrylov <47458686+AntHavrylov@users.noreply.github.com>
2025-08-27 11:08:56 +0200

    .'

--- src/app/features/manage-vocabulary/manage-vocabulary.component.ts
+++ src/app/features/manage-vocabulary/manage-vocabulary.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImportWordsComponent } from '../import-words/import-words.component';
 import { WordListComponent } from '../word-list/word-list.component';
 import { WordListManagerComponent } from '../word-list-manager/word-list-manager.component';
 import { SourceManagerComponent } from '../source-manager/source-manager.component';
+import { VocabularyVersionService } from '../../core/services/vocabulary-version.service';
+import { debounce } from 'rxjs';
 
 @Component({
   selector: 'app-manage-vocabulary',
@@ -19,9 +21,25 @@ import { SourceManagerComponent } from '../source-manager/source-manager.compone
   styleUrls: ['./manage-vocabulary.component.css']
 })
 export class ManageVocabularyComponent {
+
+  public vocabularyVersionService = inject(VocabularyVersionService);
+
   currentSubPage: string = 'word-list-manager'; // Default sub-page
+  
 
   navigateToSubPage(subPage: string): void {
     this.currentSubPage = subPage;
+  }
+
+  syncVocabulary()
+  {
+    this.vocabularyVersionService.getRemoteVersion().subscribe(remoteVersion => {
+      if (remoteVersion !== null) {
+        this.vocabularyVersionService.triggerDefaultWordsImport(remoteVersion, 'add-only');
+        
+      } else {
+        console.error('Could not get remote version to trigger vocabulary update.');
+      }
+    });
   }
 }
